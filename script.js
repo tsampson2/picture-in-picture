43f94d7b8b4366dd7e44c3d09274387ee2c82fbf
sampson2448 <teronsampson@gmail.com>
2024-05-29 14:27:51 -0400

    final commit

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("connected")
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// Prompt to select media stream video element then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch Error Hereo
+    console.log(" whoops,error here:", error);
+  }
+}
+
+button.addEventListener("click", async () => {
+  // Disable Button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  // Reset
+  button.disabled = false;
+});
+
+// On Load
+selectMediaStream();
